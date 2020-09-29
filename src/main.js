1400083a32082f42ebceb2205c84c45b208a0306
Allison Sadin <aesadin@gmail.com>
2020-09-29 12:18:23 -0700

    add mp3 to file

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-$(document).ready(function () {
-  // $('#submit-button').submit(function (event) {
-  //     event.preventDefault();
-
-  // });
+$(document).ready(function(){
+  $("#jquery_jplayer_1").jPlayer({
+    ready: function () {
+    $(this).jPlayer("setMedia", {
+      m3a: "mp3/riseUp.mp3",
+      oga: "/media/mysound.ogg"
+    });
+    },
+    swfPath: "/js",
+    supplied: "m3a, oga"
+  });
 });
